881e1ed32669d6cf7db8742632b7c4039a8d8d21
Warren Crowther <warren@velocitysoft.io>
2026-04-15 19:07:12 +1200

    Add JitPack publish support

--- mqtt-client-ws/build.gradle.kts
+++ mqtt-client-ws/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 }
 
 kotlin {
